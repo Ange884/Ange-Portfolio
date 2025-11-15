e3649d97982b1278a97324e7e9dc0f9059b9af15
Ange <ishimwenzizaangell@gmail.com>
2025-11-15 18:55:04 +0200

    .

--- src/Home/page.jsx
+++ src/Home/page.jsx
@@ -1,14 +1,22 @@
 import "../styles/home.css";
-export default function Home(){
-  return(
-    <div className="home">
-        <h1 className="main-heading">Hi I'm Nziza Ange</h1>
-        <p className="sub-heading">Specialized in crafting beautiful, responsive interfaces <br/> and smooth mobile experiences using modern technologies.</p>
-        <div className="buttons">
-          <button className="view">View my Work</button>
-          <button className="touch">Get in touch</button>
-        </div>
-    </div>
-  )
 
+export default function Home() {
+  return (
+    <section className="home" id="home">
+      <h1 className="main-heading">
+        Hi, I'm <span className="highlight">Nziza Ange</span>
+      </h1>
+
+      <p className="sub-heading">
+        Specialized in crafting beautiful, responsive
+        <br /> interfaces and delivering smooth mobile experiences
+        <br /> using modern technologies.
+      </p>
+
+      <div className="buttons">
+        <button className="view">View my work</button>
+        <button className="touch">Get in touch</button>
+      </div>
+    </section>
+  );
 }
