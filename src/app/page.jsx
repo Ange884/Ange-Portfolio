c0c52066d134cb7052f8ab63c5b268f3463f1ddf
Ange <ishimwenzizaangell@gmail.com>
2025-11-15 18:40:17 +0200

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,37 @@
 'use client';
 
 import Navbar from "@/components/Navbar";
-import Home from "@/Home/page";
+import HomeSection from "@/Home/page";
 import AboutSection from "@/About/page";
+import SkillsSection from "@/Skills/page";
+import ProjectsSection from "@/Projects/page";
+import ContactSection from "@/Contact/page";
 import "./globals.css";
 
 export default function Page() {
   return (
     <div className="relative min-h-screen overflow-hidden">
       <Navbar />
-      <Home />
-      <AboutSection />
+
+      <section id="home">
+        <HomeSection />
+      </section>
+
+      <section id="about">
+        <AboutSection />
+      </section>
+
+      <section id="skills">
+        <SkillsSection />
+      </section>
+
+      <section id="projects">
+        <ProjectsSection />
+      </section>
+
+      <section id="contact">
+        <ContactSection />
+      </section>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,14 @@
 'use client';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
 import "../styles/Navbar.css";
-import { href } from 'react-router-dom';
 
 export default function Navbar() {
-  const pathname = usePathname();
   const links = [
-    { name: 'Home', href: '/home' },
-    { name: 'About', href: '/about' },
-    {name: 'skills' , href: '/skills'},
-    { name: 'Projects', href: '/projects' },
-    { name: 'Contact', href: '/contact' },
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
   ];
 
   return (
@@ -19,12 +16,9 @@ export default function Navbar() {
       <ul className="navbar-list">
         {links.map((link) => (
           <li key={link.name}>
-            <Link 
-              href={link.href} 
-              className={`navbar-link group ${pathname === link.href ? 'active' : ''}`}
-            >
-              <span className="navbar-link-text">{link.name}</span>
-            </Link>
+            <a href={link.href} className="navbar-link">
+              {link.name}
+            </a>
           </li>
         ))}
       </ul>
