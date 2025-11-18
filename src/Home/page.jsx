ccbd6ef06d61ab9cb6e3fc6e8703bed2fee83ab8
Ange <ishimwenzizaangell@gmail.com>
2025-11-18 13:50:16 +0200

    .

--- src/Home/page.jsx
+++ src/Home/page.jsx
@@ -1,4 +1,5 @@
 import "../styles/home.css";
+import Typewriter  from "react-simple-typewriter";
 
 export default function Home() {
   return (
@@ -12,6 +13,22 @@ export default function Home() {
         <br /> <span>interfaces</span> <span>and</span> <span>delivering</span> <span>smooth</span> <span>mobile</span> <span>experiences</span>
         <br /> <span>using</span> <span>modern</span> <span>technologies.</span>
       </p>
+
+       {/* ⭐ Typewriter text */}
+      <div className="typewriter-text">
+        <Typewriter
+  options={{
+    strings: [
+      "Mobile Developer",
+      "React & React Native Developer",
+      "UI/UX Enthusiast",
+    ],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+
+      </div>
 
       <div className="buttons">
         <button className="view">View my work</button>
