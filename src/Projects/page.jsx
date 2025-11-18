3f3633037dc7d962ed9d5f1ef682258048667976
Ange <ishimwenzizaangell@gmail.com>
2025-11-18 15:39:41 +0200

    .

--- src/Projects/page.jsx
+++ src/Projects/page.jsx
@@ -12,7 +12,7 @@ export default function ProjectsSection() {
       description:
         "A full-featured e-commerce platform with real-time management and a seamless checkout experience.",
       image: "/images/luv.jpg",
-      link: "/projects/shopflow",
+      link: "https://github.com/Ange884/TradeWise-mobile.git",
       github: "https://github.com/Ange884/TradeWise-mobile.git",
       tech: "React Js",
       Category: "E-Commerce"
@@ -23,7 +23,7 @@ export default function ProjectsSection() {
       description:
         "A student and teacher portal with dashboards, role-based access, course management, and analytics.",
       image: "/images/house.jpg",
-      link: "/projects/educonnect",
+      link: "https://github.com/Ange884/House-fund-automata.git",
       github: "https://github.com/Ange884/House-fund-automata.git",
       tech: "React Native",
       Category: "E-Commerce"
@@ -34,7 +34,7 @@ export default function ProjectsSection() {
       description:
         "A health and emergency management system for supporting mothers with assistance and verified records.",
       image: "/images/mother.jpg",
-      link: "/projects/motherlink",
+      link: "https://github.com/Ange884/MotherLink-Mobile.git",
       github: "https://github.com/Ange884/MotherLink-Mobile.git",
       tech: "React Native",
       Category: "Health"
@@ -45,7 +45,7 @@ export default function ProjectsSection() {
       description:
         "A beautiful travel booking platform for flights, hotels, and trip planning with a smooth UI/UX.",
       image: "/images/hotel.jpg",
-      link: "/projects/travelease",
+      link: "https://github.com/Ange884/Travel-App-Wanderlyn-.git",
       github: "https://github.com/Ange884/Travel-App-Wanderlyn-.git",
       tech: "Next Js",
       Category: "Finance"
