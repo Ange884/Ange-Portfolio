65921825963bf45e77db795dce3b6b6cefc645f6
Ange <ishimwenzizaangell@gmail.com>
2025-12-01 19:42:57 +0200

    .

--- src/Reviews/page.jsx
+++ src/Reviews/page.jsx
@@ -41,20 +41,16 @@ export default function ReviewsSection() {
     <div className="reviewsContainer">
       <div className="left-part">
         <h1 className="thoughts">
-          What People Think About <span className="highlighted">Nziza</span>
+          What People Think <br/> About <span className="highlighted">NziZA</span>
         </h1>
 
         <p className="review-paragraph">
-          People who have worked with me often describe me as a focused, creative,
-          and determined person who always brings fresh ideas to the table. I’m known
-          for learning fast, asking thoughtful questions, and staying calm even when
-          things get challenging. My teammates appreciate how I listen carefully and try
-          to understand different perspectives before jumping into solutions. I’m also
-          recognized for my strong work ethic—when I start something, I commit fully and
-          follow it through. Teachers and peers often mention my curiosity and willingness
-          to improve, which helps me grow quickly in technical projects. Whether working
-          alone or in a team, I always aim for quality and consistency. Overall, people
-          see me as reliable, motivated, and genuinely passionate about what I do.
+         People who have worked with me describe me as creative, hardworking, and always
+          eager to learn. I'm known for asking smart questions, staying focused on my goals, 
+          and bringing a positive energy to group work. Whether I’m solving a technical problem
+           or designing something new, I'm someone who listens, improves quickly, and gives my
+            best effort. Many say they appreciate my reliability, curiosity, and teamwork, which
+             make me great to collaborate with.
         </p>
       </div>
 
